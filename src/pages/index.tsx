e39e7aa5e9d41de3fbe74804808a62b577012843
AlanWehrliLC <wehrlialan1@gmail.com>
2021-10-20 17:32:32 -0300

    starting the project

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+import styles from './home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+      <head>
+        <title>AlanWehrliLC - DoWhile 2021</title>
+      </head>
+      <h1>Welcome to Virtual Badge!</h1>
+      <h2>Alan Wehrli</h2>
+      <a href="https://www.instagram.com/sralanw/">@sralanw</a>
+    </>
+  )
+}
